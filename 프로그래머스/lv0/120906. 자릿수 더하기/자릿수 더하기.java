e3f6d3a46169045c1227ddc115dcb9027cec0325
김지민 JiminKim <wlals8369@naver.com>
2023-02-18 14:52:44 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int length = (int)(Math.log10(n)+1);
+        
+        for (int i=0; i<length; i++) {
+            answer += n%10;
+            n = n/10;
+        }
+        
+        return answer;
+    }
+}
