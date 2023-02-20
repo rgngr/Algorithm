0b6be7f4fe70f740381af9a664a4ece9f7afd4d0
김지민 JiminKim <wlals8369@naver.com>
2023-02-20 10:26:55 +0900

    [level 0] Title: 모음 제거, Time: 1.26 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        String[] arr = my_string.split("");
+        
+        for (int i=0; i<arr.length; i++) {
+            if (!arr[i].equals("a") && !arr[i].equals("e") && !arr[i].equals("i") && !arr[i].equals("o") && !arr[i].equals("u")) {
+                answer += arr[i];
+            }
+        }
+        
+        return answer;
+    }
+}
