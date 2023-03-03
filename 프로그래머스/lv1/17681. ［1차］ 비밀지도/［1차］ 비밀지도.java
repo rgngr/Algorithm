d3195dcfa3be4b309614be56f78f83d47ce3322a
김지민 JiminKim <wlals8369@naver.com>
2023-03-04 01:31:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.12 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        int[][] temp1 = new int[n][n];
+        int[][] temp2 = new int[n][n];
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                temp1[i][n-1-j] = arr1[i] % 2;
+                temp2[i][n-1-j] = arr2[i] % 2;
+                arr1[i] = arr1[i] / 2;
+                arr2[i] = arr2[i] / 2;
+            }
+        }
+        
+        for (int i=0; i<n; i++) {
+            answer[i] = "";
+            for (int j=0; j<n; j++) {
+                if ((temp1[i][j]==0) && (temp2[i][j]==0)) {
+                    answer[i] += " ";
+                } else {
+                    answer[i] += "#";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
