1b92263049fe21c3f31b8b93de573593d0c2c339
김지민 JiminKim <wlals8369@naver.com>
2023-03-04 03:41:00 +0900

    Update 올바른 괄호.java

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,6 +4,7 @@ class Solution {
     boolean solution(String s) {
         Stack st = new Stack();
         
+        // 배열로 했을 때는 효율성 테스트에서 탈락했었다..ㅠㅠ
         try {
             for (int i=0; i<s.length(); i++) {
                 if (s.charAt(i)=='(') {
