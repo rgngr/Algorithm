4e2470fc101e6301fb6e972ae48a04e5065dde55
김지민 JiminKim <wlals8369@naver.com>
2023-03-04 14:57:00 +0900

    Update 짝지어 제거하기.java

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -7,8 +7,8 @@ class Solution
         int answer = 0;
         Stack<Character> st = new Stack<>(); // 타입 지정 가능
 
-        // push()는 stack에서 제공, add()는 List에서 제공하는 메서드이다.
-        //push()의 리턴값은  <E>이고, add()의 리턴값은 boolean이다. 
+        // push()는 stack에서 제공, add()는 List에서 제공
+        // push()의 리턴값은  <E>, add()의 리턴값은 boolean 
         for (int i=0; i<s.length(); i++) {
             if (st.empty()) {
                 st.push(s.charAt(i));
