07106a3257c5f57d4725d4ab1d4f2f0ff77c77f9
김지민 JiminKim <wlals8369@naver.com>
2023-03-11 20:26:49 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (n%2 != 0) {
+            n *= 2;
+        }
+        
+        if (n%3 != 0) {
+            n *= 3;
+        }
+        
+        answer = n/6;
+        
+        return answer;
+    }
+}
