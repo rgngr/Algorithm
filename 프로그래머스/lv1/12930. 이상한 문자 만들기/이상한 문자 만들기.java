ca1b425bc96d17791cdab2be52438b91ef3c838d
김지민 JiminKim <wlals8369@naver.com>
2023-03-22 04:19:25 +0900

    Update 이상한 문자 만들기.java

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -26,4 +26,17 @@ class Solution {
         
         return answer;
     }
+    
+    // 다른 사람 풀이
+//         String answer = "";
+//         int cnt = 0;
+//         String[] array = s.split("");
+
+//         for(String ss : array) {
+//             cnt = ss.contains(" ") ? 0 : cnt + 1;
+//             answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+//         }
+//       return answer;
+//   }
 }
+
