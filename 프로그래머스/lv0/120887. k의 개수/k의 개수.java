f972eb49d3c884436ec5bef695b9bf38472950a1
김지민 JiminKim <wlals8369@naver.com>
2023-03-31 08:02:23 +0900

    Update k의 개수.java

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -18,3 +18,15 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 사람 풀이
+// class Solution {
+//     public int solution(int i, int j, int k) {
+//         String str = "";
+//         for(int a = i; a <= j; a++) {
+//             str += a+"";
+//         }
+
+//         return str.length() - str.replace(k+"", "").length();
+//     }
+// }
