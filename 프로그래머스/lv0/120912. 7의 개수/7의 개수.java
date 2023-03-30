1f1994da347b731d5bb707cde6d353940ac9fc97
김지민 JiminKim <wlals8369@naver.com>
2023-03-31 07:51:30 +0900

    [level 0] Title: 7의 개수, Time: 1.88 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -3,15 +3,14 @@ class Solution {
         int answer = 0;
         String sum = "";
         
-        String[] temp1 = new String[array.length];
+        String[] temp = new String[array.length];
         for (int i=0; i<array.length; i++) {
-            temp1[i] = Integer.toString(array[i]);
-            sum += temp1[i];
+            temp[i] = Integer.toString(array[i]);
+            sum += temp[i];
         }
         
-        String[] temp2 = sum.split("");
-        for (int i=0; i<temp2.length; i++) {
-            if (temp2[i].equals("7")) {
+        for (int i=0; i<sum.length(); i++) {
+            if (sum.charAt(i)=='7') {
                 answer++;
             }
         }
